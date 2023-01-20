962b73d9c3a7ffc0e563132af0118ce527a957b3
GDJ <GDJ@DESKTOP-J1A5EBU>
2023-01-20 15:21:22 +0900

    ej

--- GDJ56_BOB_final/src/main/java/com/today/bab/mypage/controller/MypageController.java
+++ GDJ56_BOB_final/src/main/java/com/today/bab/mypage/controller/MypageController.java
@@ -1,8 +1,34 @@
 package com.today.bab.mypage.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.today.bab.mypage.model.service.MypageService;
 
 @Controller
+@RequestMapping("/mypage")
 public class MypageController {
 
+	private MypageService service;
+	
+	//@Autowired
+	public MypageController(MypageService service) {
+		this.service=service;
+	}
+	
+//	@RequestMapping("/orderlist.do")
+//	public ModelAndView selectItemOrderList(ModelAndView mv) {
+//		//마이페이지 주문내역
+//		
+//		return mv;
+//	}
+//	
+//	@RequestMapping("/myinfo.do")
+//	public ModelAndView selectMyIfo(ModelAndView mv) {
+//		//마이페이지 내정보
+//		
+//		return mv;
+//	}
 }
