803baf7a1e3c6bee1aa65f5677ca5e32525c3ba6
GDJ <GDJ@DESKTOP-J1A5EBU>
2023-01-20 17:47:26 +0900

    EJ_아이디로 장바구니 불러오기

--- GDJ56_BOB_final/src/main/java/com/today/bab/mypage/model/dao/MypageDao.java
+++ GDJ56_BOB_final/src/main/java/com/today/bab/mypage/model/dao/MypageDao.java
@@ -1,5 +1,11 @@
 package com.today.bab.mypage.model.dao;
 
-public interface MypageDao {
+import java.util.List;
 
+import org.mybatis.spring.SqlSessionTemplate;
+
+import com.today.bab.basket.model.vo.Basket;
+
+public interface MypageDao {
+	List<Basket> selectBasketById(SqlSessionTemplate session,String userId);
 }
--- GDJ56_BOB_final/src/main/java/com/today/bab/mypage/model/service/MypageServiceImpl.java
+++ GDJ56_BOB_final/src/main/java/com/today/bab/mypage/model/service/MypageServiceImpl.java
@@ -1,8 +1,28 @@
 package com.today.bab.mypage.model.service;
 
+import java.util.List;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.today.bab.basket.model.vo.Basket;
+import com.today.bab.mypage.model.dao.MypageDao;
 
 @Service
 public class MypageServiceImpl implements MypageService {
-
+	
+	private MypageDao mypageDao;
+	private SqlSessionTemplate session;
+	
+	@Autowired
+	public MypageServiceImpl(MypageDao mypageDao,SqlSessionTemplate session) {
+		this.mypageDao=mypageDao;
+		this.session=session;
+	}
+	
+	@Override
+	public List<Basket> selectBasketById(String userId) {
+		return mypageDao.selectBasketById(session,userId);
+	}
 }
