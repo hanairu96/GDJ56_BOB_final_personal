7886a37e10b9c6dbc15b6d422bef04df2d69a45a
Jihee <mgh6958@naver.com>
2023-01-24 02:07:18 +0900

    관리자-회원관리 페이지 회원 전체 출력 기능 추가 dao

--- GDJ56_BOB_final/src/main/java/com/today/bab/admin/model/dao/AdminDaoImpl.java
+++ GDJ56_BOB_final/src/main/java/com/today/bab/admin/model/dao/AdminDaoImpl.java
@@ -1,8 +1,27 @@
 package com.today.bab.admin.model.dao;
 
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.session.RowBounds;
+import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Repository;
+
+import com.today.bab.member.model.vo.Member;
 
 @Repository
 public class AdminDaoImpl implements AdminDao{
 
+	@Override
+	public List<Member> adminMembers(SqlSessionTemplate session, Map<String, Integer> param) {
+		return session.selectList("admin.adminMembers",null, 
+	            new RowBounds((param.get("cPage")-1)*param.get("numPerpage"),
+	                  param.get("numPerpage")));
+	}
+
+	@Override
+	public int selectMemberListCount(SqlSessionTemplate session) {
+		return session.selectOne("admin.selectMemberListCount");
+	}
+	
 }
