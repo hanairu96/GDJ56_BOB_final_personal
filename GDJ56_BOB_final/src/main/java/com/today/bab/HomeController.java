beed97d2669bf55619b22a6823e911607dc120d1
dllo0 <dllo0@DESKTOP-HGKV66H>
2023-02-07 09:19:31 +0900

    헤더, 메인 화면에서 master 객체 값 사용할 수 있게 함

--- GDJ56_BOB_final/src/main/java/com/today/bab/HomeController.java
+++ GDJ56_BOB_final/src/main/java/com/today/bab/HomeController.java
@@ -6,10 +6,16 @@ import java.util.Locale;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.today.bab.admin.model.vo.AdminMaster;
+import com.today.bab.member.model.vo.Member;
+import com.today.bab.onedayclass.model.service.OneDayService;
 
 /**
  * Handles requests for the application home page.
@@ -18,6 +24,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	@Autowired
+	private OneDayService service;
 	
 	/**
 	 * Simply selects the home view to render by returning its name.
@@ -33,6 +42,19 @@ public class HomeController {
 		
 		model.addAttribute("serverTime", formattedDate );
 		
+		//로그인한 Member 객체를 받아옴
+		Object member=SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		System.out.println(member);
+		
+		//로그인한 member가 존재할 때
+		if(!member.equals("anonymousUser")) {
+			//Member의 아이디로 AdminMaster 객체 찾음
+			System.out.println(member);
+			System.out.println(((Member)member).getMemberId());
+			AdminMaster master=service.selectMastserById(((Member)member).getMemberId());
+			System.out.println(master);
+			model.addAttribute("master", master);
+		}
 		return "index";
 	}
 	
